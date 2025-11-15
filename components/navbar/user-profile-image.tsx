b0aae37a7a3fe9d907182810a6a73d9b15fd0864
robertinho_programador <guilhermemartinez39@gmail.com>
2025-11-15 17:15:52 -0300

    chore: rename navbar components for easier identification of each file's function

--- components/navbar/user-profile-image.tsx
+++ components/navbar/user-profile-image.tsx
@@ -3,9 +3,9 @@
 import { UserButton, useUser } from "@clerk/nextjs";
 import { Skeleton } from "../ui/skeleton";
 import { Button } from "../ui/button";
-import { AnimatePresence, motion } from "motion/react";
+import { motion } from "motion/react";
 
-export default function ClerkProfilePicture() {
+export default function UserProfileImage() {
     const { isLoaded } = useUser();
 
     if (!isLoaded) {
